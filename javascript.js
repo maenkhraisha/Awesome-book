0019ce82e3cd233afce24c183c88bba5dcf347ac
maen <maen.1980@yahoo.com>
2022-03-14 22:58:44 +0300

    fix linter

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-let bookListData =[];
+let bookListData = [];
 
 // === select the main containter of the book list === //
 const elBookList = document.querySelector('.book-list');
@@ -14,56 +14,54 @@ let elRemoveBtn;
 let hirozantalLine;
 
 // === create elements === //
-function createElements(){
-    elBookContainer = document.createElement('div');
-    elBookContainer.classList = 'book-container';
-    elTitle = document.createElement('p');
-    elAuthor = document.createElement('p');
-    elRemoveBtn = document.createElement('button');
-    elRemoveBtn.textContent = "remove";    
-    hirozantalLine = document.createElement('hr');
-    hirozantalLine.className = 'rounded';
+function createElements() {
+  elBookContainer = document.createElement('div');
+  elBookContainer.classList = 'book-container';
+  elTitle = document.createElement('p');
+  elAuthor = document.createElement('p');
+  elRemoveBtn = document.createElement('button');
+  elRemoveBtn.textContent = 'remove';
+  hirozantalLine = document.createElement('hr');
+  hirozantalLine.className = 'rounded';
 }
 
 // === append elements === //
-function appendElements(book){
-    elBookList.appendChild(elBookContainer);
-    elTitle.innerText = book.title;
-    elBookContainer.appendChild(elTitle);
-    elAuthor.innerText = book.author;
-    elBookContainer.appendChild(elAuthor);
-    elBookContainer.appendChild(elRemoveBtn);
-    elBookContainer.appendChild(hirozantalLine);
+function appendElements(book) {
+  elBookList.appendChild(elBookContainer);
+  elTitle.innerText = book.title;
+  elBookContainer.appendChild(elTitle);
+  elAuthor.innerText = book.author;
+  elBookContainer.appendChild(elAuthor);
+  elBookContainer.appendChild(elRemoveBtn);
+  elBookContainer.appendChild(hirozantalLine);
 
-    // === remove item from the list book === //
-    elRemoveBtn.addEventListener('click',()=>{
-        bookListData = bookListData.filter(ele => ele.title != book.title);
-        localStorage.setItem('bookList',JSON.stringify(bookListData)); 
-        window.location.reload();
-    });
+  // === remove item from the list book === //
+  elRemoveBtn.addEventListener('click', () => {
+    bookListData = bookListData.filter((ele) => ele.title !== book.title);
+    localStorage.setItem('bookList', JSON.stringify(bookListData));
+    window.location.reload();
+  });
 }
 
 // === loop on book list object === //
-function createBookList(){
-    if( localStorage.getItem('bookList') != null){
-        bookListData = JSON.parse(localStorage.getItem('bookList'));
-        
-        bookListData.forEach(book => {
-            createElements();
-            appendElements(book);       
-        });
-    }
+function createBookList() {
+  if (localStorage.getItem('bookList') != null) {
+    bookListData = JSON.parse(localStorage.getItem('bookList'));
+
+    bookListData.forEach((book) => {
+      createElements();
+      appendElements(book);
+    });
+  }
 }
 
 // === add book to book list array and localstorage === //
-elAddBtn.addEventListener('click',()=>{
-    bookListData.push({ title: elTitleInput.value, author: elAutorInput.value});
-    localStorage.setItem('bookList',JSON.stringify(bookListData)); 
+elAddBtn.addEventListener('click', () => {
+  bookListData.push({ title: elTitleInput.value, author: elAutorInput.value });
+  localStorage.setItem('bookList', JSON.stringify(bookListData));
 });
 
 /* load page */
 window.addEventListener('load', () => {
-    createBookList();
-  });
-
-
+  createBookList();
+});
