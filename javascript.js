9c2015ca6411e10f4757bb4d4aa2aaa005e2b75d
maen <maen.1980@yahoo.com>
2022-03-16 12:52:53 +0300

    refator the project to use class for add and remove books

--- javascript.js
+++ javascript.js
@@ -1,4 +1,37 @@
-let bookListData = [];
+/* eslint-disable */
+let bookArray = [];
+class BookClass {
+  constructor(id=(bookArray.length+1),title, author) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+  }
+  Add(){
+    bookArray.push(this);
+  }
+  Get(){
+    return this;
+  }
+
+  getId(){
+    return this.id;
+  }
+  getTitle() {
+    return this.title;
+  }
+
+  getAuthor() {
+    return this.author;
+  }
+
+  Remove(id) {
+    bookArray = bookArray.filter((ele) => ele.id !== this.id);
+  }
+}
+
+function fromJson(b){
+  return new BookClass(b.id,b.title,b.author);    
+}
 
 // === select the main containter of the book list === //
 const elBookList = document.querySelector('.book-list');
@@ -8,6 +41,7 @@ const elAutorInput = document.getElementById('author-input');
 
 // === declaration of variables === //
 let elBookContainer;
+let elID;
 let elTitle;
 let elAuthor;
 let elRemoveBtn;
@@ -16,6 +50,7 @@ let elRemoveBtn;
 function createElements() {
   elBookContainer = document.createElement('div');
   elBookContainer.classList = 'book-container';
+  elID = document.createElement('p');
   elTitle = document.createElement('p');
   elAuthor = document.createElement('p');
   elRemoveBtn = document.createElement('button');
@@ -25,37 +60,59 @@ function createElements() {
 // === append elements === //
 function appendElements(book) {
   elBookList.appendChild(elBookContainer);
-  elTitle.innerText = book.title;
+  
+  elID.innerText = book.getId();
+  elBookContainer.appendChild(elID);
+  
+  elTitle.innerText = `${book.getTitle()} by `;
   elBookContainer.appendChild(elTitle);
-  elAuthor.innerText = book.author;
+
+  elAuthor.innerText = book.getAuthor();
   elBookContainer.appendChild(elAuthor);
+  
   elBookContainer.appendChild(elRemoveBtn);
 
-  // === remove item from the list book === //
-  elRemoveBtn.addEventListener('click', () => {
-    bookListData = bookListData.filter((ele) => ele.title !== book.title);
-    localStorage.setItem('bookList', JSON.stringify(bookListData));
-    window.location.reload();
-  });
+// === button to remove item from the list book === //
+elRemoveBtn.addEventListener('click', () => {
+  bookArray = bookArray.filter((ele) => ele.id !== book.getId());
+  localStorage.setItem('bookList', JSON.stringify(bookArray));
+  window.location.reload();
+});
 }
 
 // === loop on book list object === //
 function createBookList() {
   if (localStorage.getItem('bookList') != null) {
-    bookListData = JSON.parse(localStorage.getItem('bookList'));
+    bookArray = JSON.parse(localStorage.getItem('bookList'));
 
-    bookListData.forEach((book) => {
+    bookArray.forEach((b) => {
+      b = fromJson(b);
       createElements();
-      appendElements(book);
+      appendElements(b);
     });
   }
 }
 
 // === add book to book list array and localstorage === //
 elAddBtn.addEventListener('click', () => {
-  bookListData.push({ title: elTitleInput.value, author: elAutorInput.value });
-  localStorage.setItem('bookList', JSON.stringify(bookListData));
+  const book = new BookClass(undefined ,elTitleInput.value, elAutorInput.value);
+  book.Add(book);
+  localStorage.setItem('bookList', JSON.stringify(bookArray));
 });
+
+// test code
+function addObj(){
+  let book = new BookClass('title test','author test');
+  book.Add();
+  localStorage.setItem('bookList', JSON.stringify(bookArray));
+}
+function restoreObj(){
+  bookArray = JSON.parse(localStorage.getItem('bookList'));
+  bookArray.forEach((b) => {
+    b = fromJson(b);
+    console.log(b.constructor.name);
+ }); 
+}
 
 /* load page */
 window.addEventListener('load', () => {
